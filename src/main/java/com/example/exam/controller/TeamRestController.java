6885dc9e01690df26d6318fda6515dfa2e37038b
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-05-30 22:47:26 +0200

    small fixes

--- src/main/java/com/example/exam/controller/TeamRestController.java
+++ src/main/java/com/example/exam/controller/TeamRestController.java
@@ -1,11 +1,15 @@
 package com.example.exam.controller;
 
 import com.example.exam.entity.Team;
-import com.example.exam.repository.TeamRepository;
+import com.example.exam.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,11 +20,18 @@ import java.util.List;
 public class TeamRestController {
 
   @Autowired
-  TeamRepository teamRepository;
+  TeamService teamService;
 
   @GetMapping("/all-teams")
   public List<Team> getAllTeam(){
-    return teamRepository.findAll();
+    return teamService.findAllTeams();
+  }
+
+  @PostMapping("/")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void postTeam(@RequestBody Team team){
+    System.out.println("bla bla");
+    teamService.saveTeam(team);
   }
 
 }
