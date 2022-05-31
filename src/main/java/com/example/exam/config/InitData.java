d8147fdbceeb04d82e760b14e4dd2ac6d8264061
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-05-31 08:42:04 +0200

    alot of innitdata

--- src/main/java/com/example/exam/config/InitData.java
+++ src/main/java/com/example/exam/config/InitData.java
@@ -34,8 +34,6 @@ public class InitData implements CommandLineRunner {
     List<Rider> riders = new ArrayList<>();
     List<Rider> riders1 = new ArrayList<>();
     List<Rider> riders2 = new ArrayList<>();
-    List<Rider> riders3 = new ArrayList<>();
-    List<Rider> riders4 = new ArrayList<>();
 
     Rider rider = new Rider();
     rider.setRiderName("Bjarne Riis");
@@ -57,7 +55,7 @@ public class InitData implements CommandLineRunner {
     Rider rider2 = new Rider();
     rider2.setRiderName("Søren Bo");
     rider2.setRiderNumber(1);
-    rider2.setRiderBirthDate(LocalDate.parse("1993-11-11"));
+    rider2.setRiderBirthDate(LocalDate.parse("1990-11-11"));
     rider2.setRiderCountry("DK");
     riders1.add(rider2);
     riderService.saveRider(rider2);
@@ -169,7 +167,7 @@ public class InitData implements CommandLineRunner {
 
     Team team1 = new Team();
     team1.setRiders(riders1);
-    team1.setTeamCountry("DK");
+    team1.setTeamCountry("GR");
     team1.setTeamName("Team 2");
     teamService.saveTeam(team1);
 
@@ -197,7 +195,7 @@ public class InitData implements CommandLineRunner {
 
     Result result2 = new Result();
     result2.setMountainPoint(0);
-    result2.setSprintPoint(6);
+    result2.setSprintPoint(2);
     result2.setRider(rider);
     result2.setStage(3);
     result2.setTime(LocalTime.parse("01:18:01"));
@@ -205,7 +203,7 @@ public class InitData implements CommandLineRunner {
 
     Result result3 = new Result();
     result3.setMountainPoint(1);
-    result3.setSprintPoint(4);
+    result3.setSprintPoint(0);
     result3.setRider(rider1);
     result3.setStage(1);
     result3.setTime(LocalTime.parse("01:39:01"));
@@ -244,37 +242,276 @@ public class InitData implements CommandLineRunner {
     resultService.saveResult(result7);
 
     Result result8 = new Result();
-    result8.setMountainPoint(3);
-    result8.setSprintPoint(4);
+    result8.setMountainPoint(0);
+    result8.setSprintPoint(0);
     result8.setRider(rider2);
     result8.setStage(3);
     result8.setTime(LocalTime.parse("01:16:11"));
     resultService.saveResult(result8);
 
-    Result result6 = new Result();
-    result6.setMountainPoint(7);
-    result6.setSprintPoint(6);
-    result6.setRider(rider2);
-    result6.setStage(1);
-    result6.setTime(LocalTime.parse("01:29:01"));
-    resultService.saveResult(result6);
+    Result result9 = new Result();
+    result9.setMountainPoint(0);
+    result9.setSprintPoint(0);
+    result9.setRider(rider3);
+    result9.setStage(1);
+    result9.setTime(LocalTime.parse("01:19:01"));
+    resultService.saveResult(result9);
 
-    Result result7 = new Result();
-    result7.setMountainPoint(7);
-    result7.setSprintPoint(2);
-    result7.setRider(rider2);
-    result7.setStage(2);
-    result7.setTime(LocalTime.parse("03:14:01"));
-    resultService.saveResult(result7);
+    Result result10 = new Result();
+    result10.setMountainPoint(0);
+    result10.setSprintPoint(2);
+    result10.setRider(rider3);
+    result10.setStage(2);
+    result10.setTime(LocalTime.parse("03:44:01"));
+    resultService.saveResult(result10);
 
-    Result result8 = new Result();
-    result8.setMountainPoint(3);
-    result8.setSprintPoint(4);
-    result8.setRider(rider2);
-    result8.setStage(3);
-    result8.setTime(LocalTime.parse("01:16:11"));
-    resultService.saveResult(result8);
+    Result result11 = new Result();
+    result11.setMountainPoint(0);
+    result11.setSprintPoint(0);
+    result11.setRider(rider3);
+    result11.setStage(3);
+    result11.setTime(LocalTime.parse("01:36:11"));
+    resultService.saveResult(result11);
 
+    Result result12 = new Result();
+    result12.setMountainPoint(2);
+    result12.setSprintPoint(0);
+    result12.setRider(rider4);
+    result12.setStage(1);
+    result12.setTime(LocalTime.parse("01:16:01"));
+    resultService.saveResult(result12);
+
+    Result result13 = new Result();
+    result13.setMountainPoint(0);
+    result13.setSprintPoint(2);
+    result13.setRider(rider4);
+    result13.setStage(2);
+    result13.setTime(LocalTime.parse("02:44:01"));
+    resultService.saveResult(result13);
+
+    Result result14 = new Result();
+    result14.setMountainPoint(0);
+    result14.setSprintPoint(0);
+    result14.setRider(rider4);
+    result14.setStage(3);
+    result14.setTime(LocalTime.parse("01:56:11"));
+    resultService.saveResult(result14);
+
+    Result result15 = new Result();
+    result15.setMountainPoint(2);
+    result15.setSprintPoint(0);
+    result15.setRider(rider5);
+    result15.setStage(1);
+    result15.setTime(LocalTime.parse("01:11:01"));
+    resultService.saveResult(result15);
+
+    Result result16 = new Result();
+    result16.setMountainPoint(7);
+    result16.setSprintPoint(2);
+    result16.setRider(rider5);
+    result16.setStage(2);
+    result16.setTime(LocalTime.parse("02:30:01"));
+    resultService.saveResult(result16);
+
+    Result result17 = new Result();
+    result17.setMountainPoint(0);
+    result17.setSprintPoint(9);
+    result17.setRider(rider5);
+    result17.setStage(3);
+    result17.setTime(LocalTime.parse("01:40:11"));
+    resultService.saveResult(result17);
+
+    Result result18 = new Result();
+    result18.setMountainPoint(2);
+    result18.setSprintPoint(1);
+    result18.setRider(rider6);
+    result18.setStage(1);
+    result18.setTime(LocalTime.parse("02:10:01"));
+    resultService.saveResult(result15);
+
+    Result result19 = new Result();
+    result19.setMountainPoint(0);
+    result19.setSprintPoint(3);
+    result19.setRider(rider6);
+    result19.setStage(2);
+    result19.setTime(LocalTime.parse("02:25:01"));
+    resultService.saveResult(result19);
+
+    Result result20 = new Result();
+    result20.setMountainPoint(3);
+    result20.setSprintPoint(2);
+    result20.setRider(rider6);
+    result20.setStage(3);
+    result20.setTime(LocalTime.parse("01:27:11"));
+    resultService.saveResult(result20);
+
+    Result result21 = new Result();
+    result21.setMountainPoint(2);
+    result21.setSprintPoint(4);
+    result21.setRider(rider7);
+    result21.setStage(1);
+    result21.setTime(LocalTime.parse("02:11:01"));
+    resultService.saveResult(result21);
+
+    Result result22 = new Result();
+    result22.setMountainPoint(0);
+    result22.setSprintPoint(0);
+    result22.setRider(rider7);
+    result22.setStage(2);
+    result22.setTime(LocalTime.parse("02:11:01"));
+    resultService.saveResult(result22);
+
+    Result result23 = new Result();
+    result23.setMountainPoint(1);
+    result23.setSprintPoint(3);
+    result23.setRider(rider7);
+    result23.setStage(3);
+    result23.setTime(LocalTime.parse("01:11:11"));
+    resultService.saveResult(result23);
+
+    Result result24 = new Result();
+    result24.setMountainPoint(4);
+    result24.setSprintPoint(4);
+    result24.setRider(rider8);
+    result24.setStage(1);
+    result24.setTime(LocalTime.parse("01:11:01"));
+    resultService.saveResult(result24);
+
+    Result result25 = new Result();
+    result25.setMountainPoint(0);
+    result25.setSprintPoint(1);
+    result25.setRider(rider8);
+    result25.setStage(2);
+    result25.setTime(LocalTime.parse("02:50:01"));
+    resultService.saveResult(result22);
+
+    Result result26 = new Result();
+    result26.setMountainPoint(1);
+    result26.setSprintPoint(4);
+    result26.setRider(rider8);
+    result26.setStage(3);
+    result26.setTime(LocalTime.parse("02:12:11"));
+    resultService.saveResult(result26);
+
+    Result result27 = new Result();
+    result27.setMountainPoint(4);
+    result27.setSprintPoint(4);
+    result27.setRider(rider9);
+    result27.setStage(1);
+    result27.setTime(LocalTime.parse("01:09:01"));
+    resultService.saveResult(result27);
+
+    Result result28 = new Result();
+    result28.setMountainPoint(0);
+    result28.setSprintPoint(1);
+    result28.setRider(rider9);
+    result28.setStage(2);
+    result28.setTime(LocalTime.parse("02:51:01"));
+    resultService.saveResult(result28);
+
+    Result result29 = new Result();
+    result29.setMountainPoint(2);
+    result29.setSprintPoint(4);
+    result29.setRider(rider9);
+    result29.setStage(3);
+    result29.setTime(LocalTime.parse("02:13:11"));
+    resultService.saveResult(result29);
+
+    Result result30 = new Result();
+    result30.setMountainPoint(2);
+    result30.setSprintPoint(3);
+    result30.setRider(rider10);
+    result30.setStage(1);
+    result30.setTime(LocalTime.parse("01:29:01"));
+    resultService.saveResult(result30);
+
+    Result result31 = new Result();
+    result31.setMountainPoint(3);
+    result31.setSprintPoint(1);
+    result31.setRider(rider10);
+    result31.setStage(2);
+    result31.setTime(LocalTime.parse("02:32:01"));
+    resultService.saveResult(result31);
+
+    Result result32 = new Result();
+    result32.setMountainPoint(2);
+    result32.setSprintPoint(5);
+    result32.setRider(rider10);
+    result32.setStage(3);
+    result32.setTime(LocalTime.parse("02:12:11"));
+    resultService.saveResult(result32);
+
+    Result result33 = new Result();
+    result33.setMountainPoint(2);
+    result33.setSprintPoint(3);
+    result33.setRider(rider11);
+    result33.setStage(1);
+    result33.setTime(LocalTime.parse("01:22:01"));
+    resultService.saveResult(result33);
+
+    Result result34 = new Result();
+    result34.setMountainPoint(3);
+    result34.setSprintPoint(1);
+    result34.setRider(rider11);
+    result34.setStage(2);
+    result34.setTime(LocalTime.parse("02:33:01"));
+    resultService.saveResult(result34);
+
+    Result result35 = new Result();
+    result35.setMountainPoint(3);
+    result35.setSprintPoint(2);
+    result35.setRider(rider11);
+    result35.setStage(3);
+    result35.setTime(LocalTime.parse("02:05:11"));
+    resultService.saveResult(result35);
+
+    Result result36 = new Result();
+    result36.setMountainPoint(1);
+    result36.setSprintPoint(4);
+    result36.setRider(rider13);
+    result36.setStage(1);
+    result36.setTime(LocalTime.parse("01:12:01"));
+    resultService.saveResult(result36);
+
+    Result result37 = new Result();
+    result37.setMountainPoint(0);
+    result37.setSprintPoint(0);
+    result37.setRider(rider13);
+    result37.setStage(2);
+    result37.setTime(LocalTime.parse("02:13:01"));
+    resultService.saveResult(result37);
+
+    Result result38 = new Result();
+    result38.setMountainPoint(3);
+    result38.setSprintPoint(2);
+    result38.setRider(rider13);
+    result38.setStage(3);
+    result38.setTime(LocalTime.parse("02:11:11"));
+    resultService.saveResult(result38);
+
+    Result result39 = new Result();
+    result39.setMountainPoint(1);
+    result39.setSprintPoint(4);
+    result39.setRider(rider14);
+    result39.setStage(1);
+    result39.setTime(LocalTime.parse("01:13:01"));
+    resultService.saveResult(result39);
+
+    Result result40 = new Result();
+    result40.setMountainPoint(1);
+    result40.setSprintPoint(2);
+    result40.setRider(rider14);
+    result40.setStage(2);
+    result40.setTime(LocalTime.parse("02:14:01"));
+    resultService.saveResult(result40);
+
+    Result result41 = new Result();
+    result41.setMountainPoint(2);
+    result41.setSprintPoint(2);
+    result41.setRider(rider14);
+    result41.setStage(3);
+    result41.setTime(LocalTime.parse("02:15:11"));
+    resultService.saveResult(result41);
 
   }
 
